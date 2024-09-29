b0fc297c52980b9d4ddd854dd56b57aca9ce96f3
SirZen476 <danielzenkin82@gmail.com>
2024-09-29 18:55:13 +0300

    problem with test function - line 169 - sizes are not the same test_loss += F.nll_loss(output, label, reduction='sum').item()  # sum up batch loss, trying to change output struct

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ def test(model):
                 data = data.view(-1, 1, 28, 28)
 
             output = model(data)
-            test_loss += F.nll_loss(output, label, reduction='sum').item()  # sum up batch loss
+            test_loss += F.nll_loss(output.data.view_as(label), label, reduction='sum').item()  # sum up batch loss
             pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
             correct += pred.eq(label.data.view_as(pred)).cpu().sum().item()# add if pred is corret
 
