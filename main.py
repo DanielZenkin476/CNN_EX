b29cb2c2d95ecf54573dda2e0b3d70ce10cfe030
SirZen476 <danielzenkin82@gmail.com>
2024-09-29 19:02:41 +0300

    #problem with test func- checking sizes, changed size of utput used in nll loss - need to check if ok, might ne another problem

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ def test(model):
                 data = data.view(-1, 1, 28, 28)
             output = model(data)
             print(f"output shape: {output.shape}, label shape: {label.shape}")
-            test_loss += F.nll_loss(output, label, reduction='sum').item()  # sum up batch loss
+            test_loss += F.nll_loss(output[:label.size(0)], label, reduction='sum').item()  # sum up batch loss
             pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
             correct += pred.eq(label.data.view_as(pred)).cpu().sum().item()# add if pred is corret
 
