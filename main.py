1010e8af04ecec33319886860b9ccd2f93463e4e
SirZen476 <danielzenkin82@gmail.com>
2024-09-29 17:18:02 +0300

    added visualization of data

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ norm_std = 0.5
 train_loader = data.DataLoader(datasets.MNIST('../data', train=True, download=True,
                    transform=transforms.Compose([transforms.ToTensor(),
                        transforms.Normalize((norm_mean,), (norm_std,))
-                   ])), batch_size=64, shuffle=True)
+                   ])), batch_size=64, shuffle=True)# shuffle to randomize , batch size 64 as defualt
 test_loader = torch.utils.data.DataLoader(datasets.MNIST('../data', train=False,
                        transform=transforms.Compose([
                        transforms.ToTensor(),
@@ -32,6 +32,16 @@ test_loader = torch.utils.data.DataLoader(datasets.MNIST('../data', train=False,
 print(train_loader.dataset.__len__())# so pycharm is telling no such function exsists- guess not
 print(test_loader.dataset.__len__())
 #image shape
-data_sample = train_loader.dataset.__getitem__(0)
+data_sample = train_loader.dataset.__getitem__(0)#gets first image
 print(f'data sample shape is: {data_sample[0].shape}, with the label: {data_sample[1]}')
 print(data_sample[0].min(), data_sample[0].max())
+
+# now to vizualize the data
+plt.figure(figsize=(16,6))
+for i in(range(10)):
+    plt.subplot(2,5,i+1)
+    image, label = train_loader.dataset.__getitem__(i)
+    plt.imshow(image.reshape(28,28), cmap='gray')
+    plt.axis('off')
+plt.show()
+
